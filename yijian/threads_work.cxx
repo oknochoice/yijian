669a51e9084c4aeda9fadc8951178d61e6fd5458
yijian <oknochoice@qq.com>
2016-10-13 16:19:26 +0800

    add threads_work

---
+++ yijian/threads_work.cxx
@@ -0,0 +1,115 @@
+#include "threads_work.h"
+
+namespace yijian {
+
+// noti threads
+noti_threads::noti_threads(uint_fast16_t thread_num)
+  : thread_count_(thread_num){
+    for (uint_fast16_t i = 0; i < thread_num; i++) {
+      auto thread_data = (Thread_Data*)malloc(sizeof(Thread_Data));
+      thread_data->thread_ = std::thread(&noti_threads::thread_func, thread_data);
+      thread_data->isContine_ = true;
+      thread_data->c_isWait_ = true;
+      thread_data->v_pingnode_sp_.reset(new Thread_Data::Vector_Node);
+      vec_threads_.push_back(thread_data);
+    }
+};
+
+noti_threads::~noti_threads() {
+  for(auto thread_data: vec_threads_) {
+    thread_data->thread_.join();
+    free(thread_data);
+  }
+}
+
+void noti_threads::sentWork(std::function<void(void)> && func,
+    PingNode * node, 
+    std::function<void(void)> && finish) {
+  bool isContine = true;
+  while(isContine) {
+
+    for (auto thread_data: vec_threads_) {
+      if (nullptr == thread_data->current_node_) {
+        thread_data->work_func_ = func;
+        thread_data->workFinish_func_ = finish;
+        thread_data->current_node_ = node;
+        isContine = false;
+        std::unique_lock<std::mutex> ul(thread_data->c_mutex_);
+        thread_data->c_isWait_ = false;
+        thread_data->c_var_.notify_one();
+        break;
+      }
+    }
+
+    if (isContine) {
+      std::unique_lock<std::mutex> ul(c_mutex_);
+      c_var_.wait(ul, [&](){
+          return !c_isWait_;
+          });
+      c_isWait_ = true;
+    }
+
+  }
+}
+
+void noti_threads::foreachio(
+    std::function<void(struct PingNode *)> && func) {
+
+  for (auto thread_data: vec_threads_) {
+    Thread_Data::Vector_Node_SP data_sp;
+    {
+      std::unique_lock<std::mutex> ul(thread_data->v_mutex_);
+      data_sp = thread_data->v_pingnode_sp_;
+    }
+    for (auto node: *data_sp) {
+      func(node);
+    }
+  }
+
+}
+
+void noti_threads::thread_func(Thread_Data* thread_data) {
+  bool isContine = true;
+  while (isContine) {
+    
+    if (thread_data->isContine_.load()){
+      {
+        std::unique_lock<std::mutex> ul(thread_data->c_mutex_);
+        thread_data->c_var_.wait(ul, [&](){
+             return !thread_data->c_isWait_;
+          });
+        thread_data->c_isWait_ = true;
+      }
+
+      thread_data->work_func_();
+
+      {
+        std::unique_lock<std::mutex> ul(thread_data->v_mutex_);
+        auto v_pingnode = thread_data->v_pingnode_sp_;
+        if (!v_pingnode.unique()) {
+          v_pingnode.reset(new Thread_Data::Vector_Node);
+        }
+        v_pingnode->push_back(thread_data->current_node_);
+        thread_data->current_node_ = nullptr;
+      }
+
+      {
+        std::unique_lock<std::mutex> ul(c_mutex_);
+        c_isWait_ = false;
+        c_var_.notify_one();
+      }
+
+      thread_data->workFinish_func_();
+
+    }else {
+      if (thread_data->v_pingnode_sp_->empty()) {
+        isContine = false;
+      }else {
+        std::this_thread::yield();
+      }
+    }
+  }
+}
+
+
+}
--- yijian/threads_work.h
+++ yijian/threads_work.h
@@ -7,28 +7,48 @@ struct PingNode;
 #include <thread>
 #include <condition_variable>
 #include <utility>
+#include <tuple>
+#include <atomic>
+#include <string>
 
 namespace yijian {
 
+struct Thread_Data {
+  typedef std::vector<PingNode*> Vector_Node;
+  typedef std::shared_ptr<Vector_Node>  Vector_Node_SP;
+
+  std::thread thread_;
+
+  std::atomic_bool isContine_;
+  std::mutex c_mutex_;
+  bool c_isWait_;
+  std::condition_variable c_var_;
+
+  std::function<void(void)> && work_func_;
+  std::function<void(void)> && workFinish_func_;
+
+  std::mutex v_mutex_;
+  Vector_Node_SP v_pingnode_sp_;
+  PingNode * current_node_;
+};
+
 class noti_threads : public noncopyable{
 public:
-  typedef std::pair<std::mutex *, 
-            std::condition_variable *>  Mutex_C_Var;
-  typedef std::pair<std::thread *, 
-            Mutex_C_Var * > Thread_Data;
 
   noti_threads(uint_fast16_t thread_num = 4);
   ~noti_threads();
+
   // push to vec_threads_
   void sentWork(std::function<void(void)> && func,
+      PingNode * node, 
       std::function<void(void)> && finish);
   void foreachio(std::function<void(struct PingNode *)> && func);
-  static void thread_func(Mutex_C_Var * mutex_c_var);
+  void thread_func(Thread_Data * thread_data);
 private:
   uint_fast16_t thread_count_;
-  std::shared_ptr<std::vector<PingNode*>> vecsp_pingnode_;
   std::vector<Thread_Data*> vec_threads_;
-  std::mutex mutex_;
+  std::mutex c_mutex_;
+  bool c_isWait_;
   std::condition_variable c_var_;
 };
 
